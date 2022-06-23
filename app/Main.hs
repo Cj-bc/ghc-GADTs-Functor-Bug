d0b2c1190e86da8b3364d7b1558f6bab86f45c55
Cj-bc <cj.bc-sd@outlook.jp>
2022-06-23 22:00:50 +0900

    Minimal reproducable code
    This code contains at least 2 errors:
    
    1. Functor instance of 'B' value constructor have two values although it only accept one.
    2. fmap implementation have wrong type:  'fmap :: (r -> b) -> TestType r a -> TestType b a'
    
    But GHC 9.0.2 paniced instead of writing.
    
    The error given was:
    
    ```
    $ stack build
    ghc-GADTs-Functor-Bug> build (lib + exe)
    Preprocessing library for ghc-GADTs-Functor-Bug-0.1.0.0..
    Building library for ghc-GADTs-Functor-Bug-0.1.0.0..
    Preprocessing executable 'ghc-GADTs-Functor-Bug-exe' for ghc-GADTs-Functor-Bug-0.1.0.0..
    Building executable 'ghc-GADTs-Functor-Bug-exe' for ghc-GADTs-Functor-Bug-0.1.0.0..
    [2 of 2] Compiling Main
    ghc: panic! (the 'impossible' happened)
      (GHC version 9.0.2:
            No skolem info:
      [b_a10w[sk:3]]
      Call stack:
          CallStack (from HasCallStack):
            callStackDoc, called at compiler/GHC/Utils/Outputable.hs:1230:37 in ghc:GHC.Utils.Outputable
            pprPanic, called at compiler/GHC/Tc/Errors.hs:2859:17 in ghc:GHC.Tc.Errors
    
    Please report this as a GHC bug:  https://www.haskell.org/ghc/reportabug
    
    --  While building package ghc-GADTs-Functor-Bug-0.1.0.0 (scroll up to its section to see the error) using:
          /home/me/Documents/ghq/github.com/Cj-bc/dotfiles/dotfiles/stack/setup-exe-cache/x86_64-linux-tinfo6/Cabal-simple_mPHDZzAJ_3.4.1.0_ghc-9.0.2 --builddir=.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.4.1.0 build lib:ghc-GADTs-Functor-Bug exe:ghc-GADTs-Functor-Bug-exe --ghc-options " -fdiagnostics-color=always"
        Process exited with code: ExitFailure 1
    ```

---
+++ app/Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE GADTs #-}
+module Main where
+
+data TestType r a where
+  A :: r -> TestType r ()
+  B :: r -> TestType r ()
+
+instance Functor (TestType r) where
+  fmap f (A a) = A (f a)
+  fmap f (B noExist r) = B (f r)
+  
+main = print "hello!"
